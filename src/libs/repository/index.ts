364f0b564b2779bb4f4165e092fa621565877843
adel.bensalem1 <adel.bensalem@augeo.org>
2021-05-30 17:57:34 +0200

    remove hard coded paths

--- src/libs/repository/index.ts
+++ src/libs/repository/index.ts
@@ -1,12 +1,17 @@
 import { Repository } from "./types";
 import { connectToHost } from "./ssh";
 
+const ansibleLocation =
+  process.env.ANSIBLE_LOCATION || "/etc/ansible/voltron-control-node";
+const repositoriesLocation =
+  process.env.REPOSITORIES_LOCATION || "/etc/ansible/voltron-control-node";
+
 const createRepository = (): Repository => ({
   saveApplication: (application) =>
     connectToHost().then((connection) =>
       connection
         .execCommand(
-          `ansible-playbook -i /etc/ansible/voltron-control-node/inventory -e "application_name=${application.name}" /etc/ansible/voltron-control-node/create-application.yml`
+          `ansible-playbook -i ${ansibleLocation}/inventory -e "application_name=${application.name}" ${ansibleLocation}/create-application.yml`
         )
         .then(() => application)
         .finally(() => connection.dispose())
@@ -14,7 +19,7 @@ const createRepository = (): Repository => ({
   findApplication: (application) =>
     connectToHost().then((connection) =>
       connection
-        .execCommand(`[ -d /srv/git/${application.name}.git ]`)
+        .execCommand(`[ -d ${repositoriesLocation}/${application.name}.git ]`)
         .then(({ code }) => (code === 1 ? null : application))
         .finally(() => connection.dispose())
     ),
