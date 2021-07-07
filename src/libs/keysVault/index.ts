91cd058e5038f2ef5a2c63547577b8d507e12ad8
adel.bensalem1 <adel.bensalem@epitech.eu>
2021-07-07 21:47:35 +0200

    fix keys vault

--- src/libs/keysVault/index.ts
+++ src/libs/keysVault/index.ts
@@ -1,12 +1,14 @@
 import { NodeSSH } from "node-ssh";
 import { KeysVault } from "@core";
 
+const host = process.env.HOST_ADDRESS;
+
 function createKeysVault(ssh: NodeSSH): KeysVault {
   return {
     createKey(user): Promise<string> {
       return ssh
         .execCommand(
-          `ssh-keygen -q -t rsa -b 4096 -N "" -f ~/.ssh/${user.email} -C "${user.email}" && cat ~/.ssh/${user.email}`
+          `ssh-keygen -q -t rsa -b 4096 -N "" -f ~/.ssh/${user.email} -C "${user.email}" && ssh-copy-id -i ~/.ssh/${user.email} git@${host} && cat ~/.ssh/${user.email}`
         )
         .then(({ stdout }) => stdout);
     },
